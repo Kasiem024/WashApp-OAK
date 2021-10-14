668b2026ac69f39e5453238ddc6c71df38246ffa
Oscar <oscargus0247@gmail.com>
2021-10-14 16:09:51 +0200

    added js login function

---
+++ public/js/login.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function Login() {
+    let inputEmail = document.getElementById('email').value;
+    let inputPas = document.getElementById('pas').value;
+
+    if (inputEmail == 'test@test.com' && inputPas == '123') {
+        window.location.href = 'index.html';
+    } else{
+        alert('That is not a valid account');
+    }
+}
